46d9d26dacdb648b26eb450215d88243089995ea
IkerLenena <ikerli@alumni.mioti.es>
2026-01-20 14:36:44 +0100

    Update clean_hf.py

--- scripts/clean_hf.py
+++ scripts/clean_hf.py
@@ -13,21 +13,27 @@ def clean_hf(
         file for file in files
         if file.startswith(bad_prefixes) or file.endswith(bad_suffixes)
     ]
-    
-    print("Archivos identificados para borrar:\n", to_delete)
+
+    if len(to_delete) == 0:
+        print("No hay nada que limpiar...")
+        return
+
+    print("Archivos identificados para borrar:\n", "\n".join(to_delete))
     user_input = input(f"Escribe '{repo_id}' para borrar archivos:")
 
-    if user_input == repo_id:
-        for file in to_delete:
-            api.delete_file(
-                repo_id = repo_id,
-                repo_type = "dataset",
-                path_in_repo = file,
-                commit_message = f"Remove non-dataset file: {file}"
-            )
-        print("Archivos borrados.")
-    else:
+    if user_input != repo_id:
         print("Cancelado.")
+        return
+
+    for file in to_delete:
+        api.delete_file(
+            repo_id = repo_id,
+            repo_type = "dataset",
+            path_in_repo = file,
+            commit_message = f"Remove non-dataset file: {file}"
+        )
+    print("Archivos borrados.")
+
 
 
 if __name__ == "__main__":
